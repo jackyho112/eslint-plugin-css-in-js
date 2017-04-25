04fa0435ffae7cfa04c93e213646c7bd660858f0
jackyho112 <jackyho112dev@gmail.com>
2017-04-25 13:10:55 -0700

    Add invalid CSS property rule

---
+++ README.md
@@ -0,0 +1,51 @@
+# eslint-plugin-685930
+
+For all your CSS JS object linting need
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-685930`:
+
+```
+$ npm install eslint-plugin-685930 --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-685930` globally.
+
+## Usage
+
+Add `685930` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "685930"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "685930/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/no-invalid-css-property.js
@@ -0,0 +1,63 @@
+
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-invalid-css-property');
+
+const RuleTester = require('eslint').RuleTester;
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+});
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const errors = [{ message: 'Invalid CSS property' }];
+const ruleTester = new RuleTester();
+ruleTester.run('no-invalid-css-property', rule, {
+  valid: [
+    // test that if there is no valid CSS property as keys, object should not
+    // be identified as a CSS object and hence not be linted
+    "var css = { sword: 'dragon', dog: 50 }",
+
+    // test for allowing media queries
+    "var css = { '@media(min-width: 300px)': { fontSize: 20 } }",
+
+    // test for allowing pseudo elements and nesting
+    "var css = { ':hover': { color: 'blue' } }",
+
+    // test for allowing special Glamor CSS object keys
+    "var css = { 'html.ie9 & span.title': { fontWeight: 'bold' } }",
+  ],
+  invalid: [
+    // should find a lint error when there is at least one valid CSS property
+    // key in the object plus any invalid CSS property key
+    { errors, code: "var css = { height: '40', ball: true }" },
+
+    // should find a lint error for invalid Pseudo selectors
+    {
+      errors,
+      code: "var css = { color: 'blue', ':after': { position: 'relative' }}",
+    },
+
+    // should find a lint error for nested invalid CSS properties
+    {
+      errors,
+      code: "var css = { color: 'blue', '::after': { diamond: 'relative' }}",
+    },
+
+    // should find a lint error when the glamor special use case that supports
+    // multiple selectors is missing &, the target selector
+    {
+      errors,
+      code: "var css = { 'html.ie9 span.title': { fontWeight: 'bold' } }",
+    },
+  ],
+});
